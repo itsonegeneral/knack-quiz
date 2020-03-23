9740c55a90b1df3ffae24d705777c63e05257882
rithiks <srithik1999@gmail.com>
2020-03-24 00:14:05 +0530

    Trending categories in home page

--- app/src/main/java/com/rstudio/knackquiz/fragments/bottomnav/FragmentHome.java
+++ app/src/main/java/com/rstudio/knackquiz/fragments/bottomnav/FragmentHome.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,13 +17,27 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.button.MaterialButton;
+import com.google.gson.Gson;
 import com.rstudio.knackquiz.HomeActivity;
+import com.rstudio.knackquiz.IntroFavouriteActivity;
 import com.rstudio.knackquiz.R;
 import com.rstudio.knackquiz.adapters.CategoryAdapter;
+import com.rstudio.knackquiz.adapters.ParentCategoryAdapter;
 import com.rstudio.knackquiz.gameplay.QuestionActivity;
 import com.rstudio.knackquiz.helpers.CategoryHelper;
+import com.rstudio.knackquiz.helpers.DBClass;
 import com.rstudio.knackquiz.models.Category;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -31,16 +46,17 @@ public class FragmentHome extends Fragment {
 
     private Context context;
     private LinearLayout layout;
-    private ArrayList<Category> favCats = new ArrayList<>();
+    private ArrayList<Category> favCats = new ArrayList<>(), trendingCats;
     private HomeActivity homeActivity;
     private static final String TAG = "FragmentHome";
-    private RecyclerView rViewFavourites;
+    private RecyclerView rViewFavourites, rViewTrending;
     private CategoryAdapter categoryAdapter;
 
-    public FragmentHome(){
+    public FragmentHome() {
         context = getActivity();
-        homeActivity = (HomeActivity)context;
+        homeActivity = (HomeActivity) context;
     }
+
     public FragmentHome(Context context) {
         this.context = context;
         homeActivity = (HomeActivity) context;
@@ -52,6 +68,8 @@ public class FragmentHome extends Fragment {
         layout = (LinearLayout) inflater.inflate(R.layout.fragment_home, container, false);
         getFavourites();
         initUI();
+
+        getTrendingCats();
 
 
         return layout;
@@ -65,11 +83,56 @@ public class FragmentHome extends Fragment {
 
     }
 
+    private void getTrendingCats() {
 
-    private void initUI(){
-        rViewFavourites =layout.findViewById(R.id.rView_favouritesHome);
-        categoryAdapter =new CategoryAdapter(homeActivity,favCats);
-        rViewFavourites.setLayoutManager(new LinearLayoutManager(getContext().getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, DBClass.urlGetCategories,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String result = jsonObject.getString("status");
+                            Gson gson = new Gson();
+                            if (result.equalsIgnoreCase("success")) {
+                                JSONArray jsonCats = jsonObject.getJSONArray("categories");
+
+                                for (int i = 0; i < jsonCats.length(); i++) {
+                                    Category category = gson.fromJson(jsonCats.getJSONObject(i).toString(), Category.class);
+                                    trendingCats.add(category);
+                                }
+
+                                rViewTrending.setLayoutManager(new LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false));
+                                categoryAdapter = new CategoryAdapter(context,trendingCats);
+                                rViewTrending.setAdapter(categoryAdapter);
+
+                            } else {
+                                //Databse error
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(context, "Er", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "ERRROR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+
+
+    private void initUI() {
+        rViewFavourites = layout.findViewById(R.id.rView_favouritesHome);
+        rViewTrending = layout.findViewById(R.id.rView_trendingCatsHome);
+        trendingCats = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(homeActivity, favCats);
+        rViewFavourites.setLayoutManager(new LinearLayoutManager(getContext().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false));
         rViewFavourites.setAdapter(categoryAdapter);
     }
 

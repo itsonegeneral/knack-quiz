452d983d099b9a826c75afdc53601ee2152c159a
rithiks <srithik1999@gmail.com>
2020-04-01 01:47:44 +0530

    Tab Layout contests

--- app/src/main/java/com/rstudio/knackquiz/fragments/bottomnav/FragmentContest.java
+++ app/src/main/java/com/rstudio/knackquiz/fragments/bottomnav/FragmentContest.java
@@ -11,10 +11,14 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.tabs.TabLayout;
 import com.rstudio.knackquiz.R;
+import com.rstudio.knackquiz.adapters.ViewPagerAdapter;
 import com.rstudio.knackquiz.gameplay.QuestionActivity;
+import com.rstudio.knackquiz.gameplay.contests.MyContestFragment;
 
 public class FragmentContest extends Fragment {
 
@@ -23,12 +27,12 @@ public class FragmentContest extends Fragment {
     private MaterialButton btnPlay; //TODO remove this
 
     public FragmentContest() {
-        context = getContext();
+        if (context == null)
+            context = getContext();
     }
 
     public FragmentContest(Context context) {
         this.context = context;
-
     }
 
     @Nullable
@@ -36,7 +40,14 @@ public class FragmentContest extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         layout = (LinearLayout) inflater.inflate(R.layout.fragment_contest, container, false);
 
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getFragmentManager());
+        TabLayout tabLayout = layout.findViewById(R.id.tabLayout_contestFrag);
+        ViewPager viewPager = layout.findViewById(R.id.viewPager_contestFrag);
 
+        viewPagerAdapter.addFrag(new MyContestFragment(context), "Contests");
+        viewPagerAdapter.addFrag(new MyContestFragment(context), "My Contests");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
         return layout;
     }
